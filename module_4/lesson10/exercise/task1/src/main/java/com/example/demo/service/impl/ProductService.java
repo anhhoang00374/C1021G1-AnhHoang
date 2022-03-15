775e5b90daaf907d0c28e4e368db1c5ea848d18a
ahhoang00374 <ahh>
2022-03-15 14:05:23 +0700

    update

---
+++ module_4/lesson10/exercise/task1/src/main/java/com/example/demo/service/impl/ProductService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.IProductRepository;
+import com.example.demo.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService implements IProductService {
+    @Autowired
+    IProductRepository productRepository;
+    @Override
+    public Page<Product> productList(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    @Override
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public void delete(Product product) {
+        productRepository.delete(product);
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+}
